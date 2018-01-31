4c125f8c5dc50c5108070bef9300841a483e866b
Per Hallsmark <per@hallsmark.se>
2018-02-01 00:48:02 +0100

    indent cleanup. multichannel. togge/level
    Signed-off-by: Per Hallsmark <per@hallsmark.se>

--- ports/arduino/libraries/Siot.h
+++ ports/arduino/libraries/Siot.h
@@ -9,7 +9,7 @@
 #include <ESP8266WiFi.h>
 
 extern "C" {
-# ifdef SECURE
+#ifdef SECURE
 #include <mbedtls/config.h>
 
 #define mbedtls_snprintf   Serial.snprintf
@@ -21,35 +21,32 @@ extern "C" {
 #include <mbedtls/sha256.h>
 #include <mbedtls/entropy.h>
 #include <mbedtls/ctr_drbg.h>
-# endif
+#endif
 
 }
 #endif
-
 #include <WiFiUdp.h>
-
-class Siot
-{
-	public:
-		Siot(void);
-		void Init(const char *mcip, const int mcport);
-		void Tx(const char *buf, const int len);
-		void Rx(const int maxlen, char *buf, int *len);
+class Siot {
+      public:
+	Siot(void);
+	void Init(const char *mcip, const int mcport);
+	void Tx(const char *buf, const int len);
+	void Rx(const int maxlen, char *buf, int *len);
 #ifdef SECURE
-		void InitSecurity(const char *privkey, const int privkey_len, const char *pubkey, const int pubkey_len);
-		int simple_encrypt_blob(mbedtls_pk_context *pk, char *buf, int buf_len, char** buf_enc, size_t* buf_enc_len);
-		int simple_decrypt_blob(mbedtls_pk_context *pk, char *buf, int buf_len, char** buf_dec, size_t* buf_dec_len);
+	void InitSecurity(const char *privkey, const int privkey_len, const char *pubkey, const int pubkey_len);
+	int simple_encrypt_blob(mbedtls_pk_context * pk, char *buf, int buf_len, char **buf_enc, size_t * buf_enc_len);
+	int simple_decrypt_blob(mbedtls_pk_context * pk, char *buf, int buf_len, char **buf_dec, size_t * buf_dec_len);
 #endif
-	private:
-		WiFiUDP _udp;
-		IPAddress _ourIP;
-		IPAddress _mcIP;
-		int _mcPort;
+      private:
+	WiFiUDP _udp;
+	IPAddress _ourIP;
+	IPAddress _mcIP;
+	int _mcPort;
 #ifdef SECURE
-		mbedtls_pk_context pkpriv;
-		mbedtls_pk_context pkpub;
-		mbedtls_entropy_context entropy;
-		mbedtls_ctr_drbg_context ctr_drbg;
+	mbedtls_pk_context pkpriv;
+	mbedtls_pk_context pkpub;
+	mbedtls_entropy_context entropy;
+	mbedtls_ctr_drbg_context ctr_drbg;
 #endif
 };
 
--- ports/arduino/load-switch/load-switch.ino
+++ ports/arduino/load-switch/load-switch.ino
@@ -5,82 +5,95 @@
 char ssid[] = "ssid";
 char pass[] = "password";
 
-char mc_ip[4] = {239,0,0,1};
+char mc_ip[4] = { 239, 0, 0, 1 };
+
 int mc_port = 2183;
 Siot siot;
 
-static int parse_set(char *message)
+#define MAX_CHANNELS 16
+
+struct sCh {
+	int load_state;
+} Ch[MAX_CHANNELS];
+
+static void parse_set(char *message)
 {
-        char *str;
-        char *strp;
-        int status;
-        char outmsg[64];
-        int ch;
-        int level;
+	char *str;
+	char *strp;
+	int status;
+	char outmsg[64];
+	int ch;
+	int level;
 
-        for (str=strtok_r(message," ", &strp); str; str=strtok_r(NULL, " ", &strp)) {
-                if (strstr(str, "channel")) {
-                        status=sscanf(str, "channel=%d", &ch);
-                        continue;
-                }
-                if (strstr(str, "level")) {
-                        sscanf(str, "level=%d", &level);
-                        continue;
-                }
-        }
+	for (str = strtok_r(message, " ", &strp); str; str = strtok_r(NULL, " ", &strp)) {
+		if (strstr(str, "channel")) {
+			status = sscanf(str, "channel=%d", &ch);
+			if (ch >= MAX_CHANNELS) {
+				return;
+			}
+			continue;
+		}
+		if (strstr(str, "level")) {
+			sscanf(str, "level=%d", &level);
+			Ch[ch].load_state = level;
+			continue;
+		} else if (strstr(str, "toggle")) {
+			Ch[ch].load_state = !(Ch[ch].load_state);
+			level = Ch[ch].load_state;
+			continue;
+		}
+	}
 
-        if (level) {
-                /* enable mosfet */
-                digitalWrite(LED_BUILTIN, LOW);
-        } else {
-                /* disable mosfet */
-                digitalWrite(LED_BUILTIN, HIGH);
-        }
+	if (level) {
+		/* enable mosfet, simulate via onboard led */
+		digitalWrite(LED_BUILTIN, LOW);
+	} else {
+		/* disable mosfet, simulate via onboard led */
+		digitalWrite(LED_BUILTIN, HIGH);
+	}
 
-        /* publish status */
-        status=sprintf(outmsg, "/load/switch/status channel=%d level=%d", ch, level);
-        siot.Tx(outmsg, status+1);
+	/* publish status */
+	status = sprintf(outmsg, "/load/switch/status channel=%d level=%d", ch, level);
+	siot.Tx(outmsg, status + 1);
 }
 
-void setup() {
-  int status;
-  
-  Serial.begin(115200);
+void setup()
+{
+	int status;
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
+	Serial.begin(115200);
 
-  while ((status = WiFi.status()) != WL_CONNECTED) {
-    delay(1000);
-  }
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, pass);
 
-  String APs = WiFi.BSSIDstr();
-  char AP[80];
-  
-  APs.toCharArray(AP, sizeof(AP));
+	while ((status = WiFi.status()) != WL_CONNECTED) {
+		delay(1000);
+	}
 
-  Serial.printf("connected to AP %s, signal strength %d\n", AP, WiFi.RSSI());
+	String APs = WiFi.BSSIDstr();
+	char AP[80];
 
-  siot.Init(mc_ip, mc_port);
+	APs.toCharArray(AP, sizeof(AP));
 
-  pinMode(LED_BUILTIN, OUTPUT);
+	Serial.printf("connected to AP %s, signal strength %d\n", AP, WiFi.RSSI());
+
+	siot.Init(mc_ip, mc_port);
+
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  char buf[512];
-  int len;
-  
-  // check for siot messages
-  siot.Rx(sizeof(buf), buf, &len);
-  if (len) {
-    if (strstr(buf, "/load/switch/set")) {
-      parse_set(buf);
-    }
-    len=0;
-    memset(buf, 512, 0);
-  }
+void loop()
+{
+	char buf[512];
+	int len;
+
+	// check for siot messages
+	siot.Rx(sizeof(buf), buf, &len);
+	if (len) {
+		if (strstr(buf, "/load/switch/set")) {
+			parse_set(buf);
+		}
+		len = 0;
+		memset(buf, 512, 0);
+	}
 }
-
-
-
-
